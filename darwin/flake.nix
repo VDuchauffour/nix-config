a91af6127d307a6280c7455f6b20421d39ae7794
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-07-16 14:22:42 +0200

    add user infos

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -8,36 +8,43 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+    let
+      # homebrewPackages = import ./homebrew.nix;
+      vars = {
+        user = "k";
+        computerName = "k-MacBook-Pro";
+        terminal = "alacritty";
+        editor = "nvim";
+      };
+      configuration = { pkgs, ... }: {
+        nix.settings.experimental-features = "nix-command flakes";
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 6;
+        nixpkgs.hostPlatform = "aarch64-darwin";
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        users.users.k = {
+          home = "/Users/${vars.user}";
+          name = "${vars.user}";
+        };
+        system.primaryUser = vars.user;
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages =
+          [
+            pkgs.vim
+          ];
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations.${vars.computerName} = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [ configuration ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."k-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
