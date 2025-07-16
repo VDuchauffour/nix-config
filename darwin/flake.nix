67435b5dbfe8102ca1b2859a4c66847a749b53a6
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-07-16 16:58:56 +0200

    Add fonts

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -40,25 +40,27 @@
         # $ nix-env -qaP | grep wget
         environment.systemPackages = (import ../common/packages { inherit pkgs; }).cli;
 
-        homebrew = {
-          enable = true;
-          onActivation = {
-            autoUpdate = true;
-            upgrade = false;
-            cleanup = "uninstall";
-          };
-          brewPrefix = "/opt/homebrew/bin";
-          caskArgs = {
-            no_quarantine = true;
-          };
-          casks = homebrewPackages.casks;
-          brews = homebrewPackages.brews;
-          taps = homebrewPackages.taps;
-          masApps = homebrewPackages.masApps;
+        fonts.packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts)
+
+          homebrew = {
+        enable = true;
+        onActivation = {
+          autoUpdate = true;
+          upgrade = false;
+          cleanup = "uninstall";
         };
+        brewPrefix = "/opt/homebrew/bin";
+        caskArgs = {
+          no_quarantine = true;
+        };
+        casks = homebrewPackages.casks;
+        brews = homebrewPackages.brews;
+        taps = homebrewPackages.taps;
+        masApps = homebrewPackages.masApps;
       };
-    in
-    {
+      };
+      in
+      {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#simple
       darwinConfigurations.${vars.computerName} = nix-darwin.lib.darwinSystem {
@@ -75,5 +77,5 @@
       };
       # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations.${vars.computerName}.pkgs;
-    };
-}
+      };
+      }
