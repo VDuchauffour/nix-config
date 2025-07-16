57f9af0f3663df12ec676d4eaf0067bddfcedb28
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-07-16 15:53:00 +0200

    Add system default

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -27,6 +27,9 @@
           name = "${vars.user}";
         };
         system.primaryUser = vars.user;
+
+        system.defaults = (import ./system.nix { inherit vars; }).defaults;
+
         # List packages installed in system profile. To search by name, run:
         # $ nix-env -qaP | grep wget
         environment.systemPackages =
