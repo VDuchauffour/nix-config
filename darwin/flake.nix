5f1183de7c85b0b59acf15ba15ded52edbde7663
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-07-17 11:44:12 +0200

    add activation for apps symlinks

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -26,6 +26,7 @@
     configuration = {
       lib,
       pkgs,
+      config,
       ...
     }: {
       nix.settings.experimental-features = "nix-command flakes";
@@ -49,6 +50,25 @@
         launchctl stop com.apple.Dock.agent
         launchctl start com.apple.Dock.agent
       '';
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
 
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
