1eb06454529a5b4f7a30025a8ba06d8b8b60026f
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-07-18 12:51:54 +0200

    Update nixos config

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Minimal NixOS flake";
+  description = "My NixOS configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -9,31 +9,79 @@
     };
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
     home-manager,
-  } @ inputs: let
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    packages = import ../common/packages {inherit pkgs;};
+    vars = {
+      user = "k";
+      computerName = "nixos";
+      terminal = "alacritty";
+      editor = "nvim";
+    };
+    configuration = {
+      lib,
+      pkgs,
+      config,
+      ...
+    }: {
+      nix.settings.experimental-features = "nix-command flakes";
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 6;
+      nixpkgs.hostPlatform = ${system};
+      nixpkgs.config.allowUnfree = true;
+
+      users.users.k = {
+        home = "/home/${vars.user}";
+        name = "${vars.user}";
+      };
+
+      packages = import ../common/packages {inherit pkgs;};
+      environment.systemPackages =
+        packages.cli
+        ++ packages.gui
+        ++ [
+          playerctl
+          brightnessctl
+          pavucontrol
+        ];
+
+      fonts = {
+        packages = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+      };
+
+      environment.variables = rec {
+        EDITOR = "${vars.editor}";
+        VISUAL = "${vars.editor}";
+      };
+    };
   in {
-    environment.systemPackages =
-      packages.cli
-      ++ packages.gui
-      ++ [
-        playerctl
-        brightnessctl
-        pavucontrol
-      ];
+      homeConfigurations.${vars.computerName} = home-manager.lib.homeManagerConfiguration {
+        system = ${system};
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          configuration
+          home-manager.nixosModules.home-manager
+          (
+            {
+              lib,
+              pkgs,
+              config,
+              inputs,
+              ...
+            }: {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.k = import ./home.nix {inherit config lib vars pkgs;};
+              home-manager.backupFileExtension = "backup";
+            }
+          )
+        ];
+      };
 
-    # homeConfigurations = {
-    #   "k@nixos" = home-manager.lib.homeManagerConfiguration {
-    #     pkgs = nixpkgs.legacyPackages.${system};
-    #     modules = [./home.nix];
-    #   };
-    # };
-
-    # dans user ajouté docker comme groupe
+      # dans user ajouté docker comme groupe
   };
 }
--- nixos/home.nix
+++ nixos/home.nix
@@ -1,24 +1,86 @@
 {
   config,
+  lib,
   pkgs,
+  vars,
   ...
 }: {
-  home = {
-    username = "k";
-    homeDirectory = "/home/k";
-    stateVersion = "25.05";
-  };
-
   programs.home-manager.enable = true;
 
-  programs.git = import ../modules/home/git.nix {inherit pkgs;};
-  programs.zsh = import ../modules/home/zsh.nix {inherit pkgs;};
-  programs.direnv = import ../modules/home/direnv.nix {inherit pkgs;};
-  programs.firefox = import ../modules/home/firefox.nix {inherit pkgs;};
-  programs.librewolf = import ../modules/home/librewolf.nix {inherit pkgs;};
-  programs.nautilus-open-any-terminal = import ../modules/home/nautilus.nix {inherit pkgs;};
+  home.username = "${vars.user}";
+  home.homeDirectory = "/home/${vars.user}";
+  home.stateVersion = "25.05";
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
+  home.packages = [];
+
+  home.file = {
+    ".config/nvim" = {
+      source = ../dots/astronvim;
+      recursive = true;
+    };
+    ".config/lazygit" = {
+      source = ../dots/lazygit;
+    };
+    ".config/k9s" = {
+      source = ../dots/k9s;
+      recursive = true;
+    };
+    ".config/ranger" = {
+      source = ../dots/ranger;
+      recursive = true;
+    };
+    ".tmux.conf" = {
+      source = ../dots/tmux/.tmux.conf;
+    };
+
+    ".config/dunst" = {
+      source = ../dots/dunst/dunstrc;
+    };
+    ".config/gsimplecal" = {
+      source = ../dots/gsimplecal/config;
+    };
+    ".config/hypr" = {
+      source = ../dots/hypr;
+      recursive = true;
+    };
+    ".config/libinput-gestures.conf" = {
+      source = ../dots/libinput-gestures/libinput-gestures.conf;
+    };
+    ".config/mpv" = {
+      source = ../dots/mpv;
+      recursive = true;
+    };
+    ".config/rofi" = {
+      source = ../dots/rofi;
+      recursive = true;
+    };
+    ".config/waybar" = {
+      source = ../dots/waybar;
+      recursive = true;
+    };
+  };
+
+  programs = {
+    zsh =
+      import ../common/modules/home/zsh.nix
+      // {
+        initContent = ''
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+        '';
+      };
+    starship = import ../common/modules/home/starship.nix;
+    git = import ../common/modules/home/git.nix;
+    direnv = import ../common/modules/home/direnv.nix;
+    alacritty = import ../common/modules/home/alacritty.nix;
+    bottom = import ../common/modules/home/bottom.nix;
+    btop = import ../common/modules/home/btop.nix;
+    firefox = import ../common/modules/home/gecko/firefox.nix;
+    librewolf = import ../common/modules/home/gecko/librewolf.nix {pkgs = pkgs;};
+
+    nautilus = import ../common/modules/home/nautilus.nix;
+  };
+
+  xdg = {
+    enable = true;
   };
 }
