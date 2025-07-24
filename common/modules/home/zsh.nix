cb8e124b6b52cafd4786d2dbc0e540f464304f8e
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-07-24 16:04:22 +0200

    Update zsh initContent

--- common/modules/home/zsh.nix
+++ common/modules/home/zsh.nix
@@ -14,6 +14,9 @@
   initContent = ''
     eval "$(starship init zsh)"
 
+    # Load personal secrets
+    [[ ! -f ~/.envrc ]] || source ~/.envrc
+
     ksh() { kubectl exec --stdin --tty "$1" -- /bin/bash; }
   '';
 
