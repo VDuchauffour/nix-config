731884716a3037223fba27306dee83fbe519990d
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-07-28 13:24:44 +0200

    add finder user pref

--- darwin/system.nix
+++ darwin/system.nix
@@ -14,6 +14,14 @@
       TrackpadThreeFingerDrag = false;
     };
     CustomUserPreferences = {
+      "com.apple.finder" = {
+        ShowExternalHardDrivesOnDesktop = true;
+        ShowHardDrivesOnDesktop = true;
+        ShowMountedServersOnDesktop = true;
+        ShowRemovableMediaOnDesktop = true;
+        _FXSortFoldersFirst = true;
+        FXDefaultSearchScope = "SCcf"; # When performing a search, search the current folder by default
+      };
       # Cursor
       "com.todesktop.230313mzl4w4u92" = {
         "ApplePressAndHoldEnabled" = false;
