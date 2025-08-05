03f989988a27947e9898cbd2f130b002b420b59e
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-08-05 15:23:06 +0200

    chore: add some XDG dirs

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -70,6 +70,16 @@
       environment.variables = rec {
         EDITOR = "${vars.editor}";
         VISUAL = "${vars.editor}";
+
+        XDG_CACHE_HOME = "$HOME/.cache";
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_DATA_HOME = "$HOME/.local/share";
+        XDG_STATE_HOME = "$HOME/.local/state";
+
+        XDG_BIN_HOME = "$HOME/.local/bin";
+        #PATH = [
+        #  "${XDG_BIN_HOME}"
+        #];
       };
     };
   in {
