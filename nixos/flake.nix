07ea297d4464ea973734f11dd7c92b19e316880c
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2025-08-05 22:20:07 +0200

    Update nixOS config (#3)
    * Update nixos config
    
    * fix
    
    * fix
    
    * Revert "fix"
    
    This reverts commit dddd9a8318ea9e8f136c6e8df66f64c015dfd0bb.
    
    * fix
    
    * fix
    
    * Revert "fix"
    
    This reverts commit c4a795a961d34e7fabd07e54982bf379b0c52ad5.
    
    * test
    
    * test
    
    * comment nautilus
    
    * Disable resistFingerprinting

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Minimal NixOS flake";
+  description = "My NixOS configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -9,31 +9,64 @@
     };
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
     home-manager,
-  } @ inputs: let
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     packages = import ../common/packages {inherit pkgs;};
+    vars = {
+      user = "k";
+      computerName = "nixos";
+      terminal = "alacritty";
+      editor = "nvim";
+    };
   in {
-    environment.systemPackages =
-      packages.cli
-      ++ packages.gui
-      ++ [
-        playerctl
-        brightnessctl
-        pavucontrol
+    nixosConfigurations.${vars.computerName} = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        {
+          nix.settings.experimental-features = "nix-command flakes";
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          system.stateVersion = "25.05";
+          nixpkgs.hostPlatform = "x86_64-linux";
+          nixpkgs.config.allowUnfree = true;
+
+          users.users.k = {
+            home = "/home/${vars.user}";
+            name = "${vars.user}";
+            isNormalUser = true;
+            extraGroups = ["wheel" "docker"];
+          };
+
+          environment.systemPackages =
+            packages.cli
+            ++ packages.gui
+            ++ [
+              pkgs.playerctl
+              pkgs.brightnessctl
+              pkgs.pavucontrol
+            ];
+
+          fonts = {
+            packages = builtins.filter nixpkgs.lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+          };
+
+          environment.variables = rec {
+            EDITOR = "${vars.editor}";
+            VISUAL = "${vars.editor}";
+          };
+        }
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.k = {config, lib, pkgs, ...}: import ./home.nix {inherit config lib pkgs vars;};
+          home-manager.backupFileExtension = "backup";
+        }
       ];
-
-    # homeConfigurations = {
-    #   "k@nixos" = home-manager.lib.homeManagerConfiguration {
-    #     pkgs = nixpkgs.legacyPackages.${system};
-    #     modules = [./home.nix];
-    #   };
-    # };
-
-    # dans user ajouté docker comme groupe
+    };
   };
 }
