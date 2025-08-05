019326bec430ba760c4408033ea56549355c971a
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-08-05 20:38:35 +0200

    chore: remove redondant xdg envvars

--- common/modules/home/zsh.nix
+++ common/modules/home/zsh.nix
@@ -50,11 +50,6 @@
     EDITOR = "${vars.editor}";
     VISUAL = "${vars.editor}";
 
-    XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_STATE_HOME = "$HOME/.local/state";
-
     XDG_BIN_HOME = XDG_BIN_HOME;
     PATH = "${XDG_BIN_HOME}:$PATH";
   };
