37a0c3bcaf2d2ce715065589e57bae92afc28976
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-09-23 16:54:20 +0200

    disable fonts

--- machines/nixos/deckard/home.nix
+++ machines/nixos/deckard/home.nix
@@ -53,12 +53,12 @@
   fonts = {
     fontconfig = {
       enable = true;
-      defaultFonts = {
-        emoji = ["Noto Color Emoji"];
-        monospace = ["MesloLGL Nerd Font Mono"];
-        sansSerif = ["MesloLGL Nerd Font"];
-        serif = ["Times New Roman"];
-      };
+      # defaultFonts = {
+      #   emoji = ["Noto Color Emoji"];
+      #   monospace = ["MesloLGL Nerd Font Mono"];
+      #   sansSerif = ["MesloLGL Nerd Font"];
+      #   serif = ["Times New Roman"];
+      # };
     };
   };
 
