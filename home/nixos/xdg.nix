a9dc1f5e1c4d6a9151d5ae894df70407a9b39fec
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-10-07 21:18:01 +0200

    feat: use eog as default app for image viewing

--- home/nixos/xdg.nix
+++ home/nixos/xdg.nix
@@ -18,6 +18,14 @@
         "x-scheme-handler/about" = "librewolf.desktop";
         "x-scheme-handler/unknown" = "librewolf.desktop";
         "application/pdf" = "papers.desktop";
+        "image/jpeg" = "org.gnome.eog.desktop";
+        "image/png" = "org.gnome.eog.desktop";
+        "image/tiff" = "org.gnome.eog.desktop";
+        "image/bmp" = "org.gnome.eog.desktop";
+        "image/gif" = "org.gnome.eog.desktop";
+        "image/x-icon" = "org.gnome.eog.desktop";
+        "image/svg+xml" = "org.gnome.eog.desktop";
+        "image/webp" = "org.gnome.eog.desktop";
       };
     };
   };
