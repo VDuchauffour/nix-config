d4daf72706a911207b8b471128df382ae3246957
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-01 18:32:25 +0100

    move fonts to nixos desktop

--- modules/system/nixos-desktop.nix
+++ modules/system/nixos-desktop.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./display
+    ./fonts
     ./nautilus
     ./steam
   ];
--- modules/system/nixos.nix
+++ modules/system/nixos.nix
@@ -4,7 +4,6 @@
     ./bluetooth
     ./bootloader
     ./docker
-    ./fonts
     ./keyring
     ./locale
     ./network
