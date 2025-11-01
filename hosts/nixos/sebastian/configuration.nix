6991e9461bc92b3ca5d17add3242021702523762
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-01 16:23:06 +0100

    add other params

--- hosts/nixos/sebastian/configuration.nix
+++ hosts/nixos/sebastian/configuration.nix
@@ -9,6 +9,11 @@
 
   networking.hostId = "94e6e4ea";
 
+  boot.loader.grub.zfsSupport = true;
+  boot.supportedFilesystems = ["zfs"];
+  boot.zfs.extraPools = ["storage"];
+  boot.kernelParams = ["zfs.zfs_arc_max=17179869184"]; # 16GB of RAM for ARC cache
+
   users.users."${vars.userName}" = {
     home = "/home/${vars.userName}";
     name = "${vars.userName}";
@@ -20,5 +25,7 @@
 
   imports = [
     ./hardware.nix
+    ./samba-storage.nix
+    ./smartd-devices.nix
   ];
 }
