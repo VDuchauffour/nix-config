4bab57b2ff0a15266821be0bcea7dd0f493a47b1
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-02 22:37:03 +0100

    fix typo

--- hosts/nixos/sebastian/ups.nix
+++ hosts/nixos/sebastian/ups.nix
@@ -1,6 +1,6 @@
 {config, ...}: {
   power.ups = {
-    users."nut-admin".passwordFile = config.age.secrets.upsPassword.path;
-    upsmon.monitor."UPS-1".passwordFile = config.age.secrets.upsPassword.path;
+    users."nut-admin".passwordFile = config.age.secrets.upsPassword.file;
+    upsmon.monitor."UPS-1".passwordFile = config.age.secrets.upsPassword.file;
   };
 }
--- modules/system/ups/default.nix
+++ modules/system/ups/default.nix
@@ -31,7 +31,7 @@
       ];
     };
     users."nut-admin" = {
-      passwordFile = lib.mkDefault "password";
+      passwordFile = lib.mkDefault "";
       upsmon = "primary";
     };
     upsmon = {
@@ -39,7 +39,7 @@
         system = "UPS-1@localhost";
         powerValue = 1;
         user = "nut-admin";
-        passwordFile = lib.mkDefault "password";
+        passwordFile = lib.mkDefault "";
         type = "primary";
       };
       settings = {
