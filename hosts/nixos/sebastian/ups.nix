42b58f6d68806debdf79d728a4f6d8a18fb58d62
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-02 22:30:58 +0100

    fix typo

--- hosts/nixos/sebastian/ups.nix
+++ hosts/nixos/sebastian/ups.nix
@@ -1,6 +1,6 @@
 {config, ...}: {
   power.ups = {
-    users."nut-admin".passwordFile = config.age.secrets.upsPassword;
-    upsmon.monitor."UPS-1".passwordFile = config.age.secrets.upsPassword;
+    users."nut-admin".passwordFile = config.age.secrets.upsPassword.path;
+    upsmon.monitor."UPS-1".passwordFile = config.age.secrets.upsPassword.path;
   };
 }
