f7dd408b74465ed5803cf53b1a8b8b43a74e5af3
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-03 01:16:40 +0100

    fix typo

--- hosts/nixos/sebastian/configuration.nix
+++ hosts/nixos/sebastian/configuration.nix
@@ -14,6 +14,7 @@
   # boot.zfs.extraPools = ["tank"];
   boot.kernelParams = ["zfs.zfs_arc_max=17179869184"]; # 16GB of RAM for ARC cache
 
+  # we need to declare manually a legacy mountpoint
   fileSystems."/mnt/tank" = {
     device = "tank";
     fsType = "zfs";
