6344a3d975923e7355eb1d670f5cee20b87ad14e
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-04 23:28:24 +0100

    chore: cap the cpu and ram for the k3s stack

--- modules/system/k3s/default.nix
+++ modules/system/k3s/default.nix
@@ -18,8 +18,16 @@ in {
   services.k3s.enable = true;
   services.k3s.role = "server";
 
+  # Define a slice that limits CPU/RAM for the whole k3s stack
+  systemd.slices."k3s.slice".sliceConfig = {
+    MemoryMax = "12G";
+    CPUQuota = "800%"; # 8 vCPU, use nproc to see how many vCPU the machine has
+  };
+  # Ensure both services run inside that slice
+  systemd.services.k3s.serviceConfig.Slice = "k3s.slice";
+  systemd.services.containerd.serviceConfig.Slice = "k3s.slice";
+
   system.activationScripts.k3sKubeconfig = {
-    # Optionally depend on something that ensures /etc is populated
     deps = ["etc"];
     text = ''
       set -eu
