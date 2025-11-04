f58723322dffebe6e245cc29d12846953e80a00e
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-04 15:44:49 +0100

    chore: add kubeconfig generation for remote connections

--- modules/system/k3s/default.nix
+++ modules/system/k3s/default.nix
@@ -1,6 +1,14 @@
 # see https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/cluster/k3s/docs/USAGE.md
-{vars, ...}: let
+{
+  pkgs,
+  vars,
+  ...
+}: let
   kubeDir = "/home/${vars.userName}/.kube";
+  cmpBin = "${pkgs.diffutils}/bin/cmp";
+  ipBin = "${pkgs.iproute2}/bin/ip";
+  awkBin = "${pkgs.gawk}/bin/awk";
+  sedBin = "${pkgs.gnused}/bin/sed";
 in {
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
@@ -16,13 +24,24 @@ in {
     text = ''
       set -eu
       install -d -m 700 ${kubeDir}
-      # Copy only if changed
-      if [ ! -f ${kubeDir}/config ] || ! cmp -s /etc/rancher/k3s/k3s.yaml ${kubeDir}/config; then
+
+      if [ ! -f ${kubeDir}/config ] || ! ${cmpBin} -s /etc/rancher/k3s/k3s.yaml ${kubeDir}/config; then
         cp /etc/rancher/k3s/k3s.yaml ${kubeDir}/config
-        chown ${vars.userName}:users -R ${kubeDir}
         chmod 755 ${kubeDir}
         chmod 600 ${kubeDir}/config
       fi
+
+      # Compute primary IPv4 via routing table (no DNS needed)
+      if ${ipBin} -o -4 route get 1.1.1.1 >/dev/null 2>&1; then
+        PRIMARY_IP="$(${ipBin} -o -4 route get 1.1.1.1 | ${awkBin} '{for(i=1;i<=NF;i++){if($i=="src"){print $(i+1); exit}}}')"
+        if [ -n "$PRIMARY_IP" ]; then
+          cp ${kubeDir}/config ${kubeDir}/config.remote
+          ${sedBin} -i "s#server: https://127.0.0.1:6443#server: https://$PRIMARY_IP:6443#" ${kubeDir}/config.remote
+          chmod 600 ${kubeDir}/config.remote
+        fi
+      fi
+
+      chown ${vars.userName}:users -R ${kubeDir}
     '';
   };
 }
