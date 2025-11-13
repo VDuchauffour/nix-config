fbcade58c14b1dcfdf70cf190814137a40566c78
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-13 10:09:20 +0100

    chore: remove redundant portals

--- modules/user/xdg/default.nix
+++ modules/user/xdg/default.nix
@@ -8,8 +8,6 @@
     portal = {
       extraPortals = with pkgs; [
         xdg-desktop-portal-hyprland
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
         gnome-keyring
       ];
     };
