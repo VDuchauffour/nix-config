2872034c7c6e8ad38db018e23e0827e859d05b99
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-17 17:50:19 +0100

    chore: add wallace host

---
+++ hosts/nixos/wallace/configuration.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  vars,
+  ...
+}: {
+  system.stateVersion = "25.05";
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  networking.hostId = "6b0ae97a";
+
+  users.users."${vars.userName}" = {
+    home = "/home/${vars.userName}";
+    name = "${vars.userName}";
+    isNormalUser = true;
+    extraGroups = ["wheel" "docker" "audio" "video" "users" "input" "networkmanager"];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHoPCo3E+ATI/9Fcjgsv5lW2z79E1BpmOyjmGIDPllV0"
+    ];
+  };
+  programs.zsh.enable = true;
+
+  imports = [
+    ./hardware.nix
+  ];
+}
