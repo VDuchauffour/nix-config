08eef054a495374fd259608ce80522520438c684
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-11-25 14:14:28 +0100

    fix: xdgOpenUsePortal

--- modules/user/xdg/default.nix
+++ modules/user/xdg/default.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: {
   xdg = {
     enable = true;
-    xdgOpenUsePortal = true;
     userDirs = {
       enable = true;
       createDirectories = true;
     };
     portal = {
+      xdgOpenUsePortal = true;
       extraPortals = with pkgs; [
         xdg-desktop-portal-hyprland
         gnome-keyring
