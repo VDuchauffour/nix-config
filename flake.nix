6fb67f7e3f50d1cab520288293cd1c97636e15c5
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-02 15:45:06 +0100

    fix: fix substituters and trusted keys

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "My unified Nix configuration";
 
   nixConfig = {
-    trusted-substituters = [
+    extra-trusted-substituters = [
       "https://cachix.cachix.org"
       "https://nixpkgs.cachix.org"
       "https://nix-community.cachix.org"
       "https://vicinae.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-public-keys = [
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
