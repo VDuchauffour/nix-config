8815998b98e4376d2e4f09c1e0848e720c1dee5c
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-02 16:24:33 +0100

    fix: fix substituters and trusted keys

--- flake.nix
+++ flake.nix
@@ -1,22 +1,6 @@
 {
   description = "My unified Nix configuration";
 
-  nixConfig = {
-    extra-trusted-substituters = [
-      "https://cachix.cachix.org"
-      "https://nixpkgs.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://vicinae.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-      "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-    ];
-  };
-
   inputs = {
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     disko = {
--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -16,6 +16,19 @@
       experimental-features = "nix-command flakes";
       trusted-users = ["root" "${vars.userName}"];
       allowed-users = ["${vars.userName}"];
+      extra-trusted-substituters = [
+        "https://cachix.cachix.org"
+        "https://nixpkgs.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://vicinae.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+      ];
     };
     gc = {
       automatic = true;
