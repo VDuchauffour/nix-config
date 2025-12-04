3ff7949b77d5345b20a1e13ce6986cfe922eea6a
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-04 20:55:09 +0100

    chore: comment out the CPU limitation when building a config

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -13,8 +13,8 @@
 
   nix = {
     settings = {
-      max-jobs = 4;
-      cores = 4;
+      # max-jobs = 4;
+      # cores = 4;
       experimental-features = "nix-command flakes";
       trusted-users = ["root" "${vars.userName}"];
       allowed-users = ["${vars.userName}"];
