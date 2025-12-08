f07cbb807e8447082f13aa4b5763a4dc92b70b75
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-08 17:20:55 +0100

    chore: improve config

--- modules/user/ghostty/default.nix
+++ modules/user/ghostty/default.nix
@@ -8,6 +8,13 @@
       cursor-style = "block";
       cursor-style-blink = true;
       shell-integration-features = "no-cursor";
+      copy-on-select = "clipboard";
+      selection-clear-on-copy = false;
+      font-family = "JetBrainsMonoNL Nerd Font";
+      font-style = "Regular";
+      font-style-bold = "Bold";
+      font-style-italic = "Italic";
+      font-style-bold-italic = "SemiBold Italic";
     };
   };
 }
