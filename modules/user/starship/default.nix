a8d6a4b1104b14dcd8e45de29b1db3fd93e44c19
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-08 14:35:36 +0100

    chore: upadate starship config

--- modules/user/starship/default.nix
+++ modules/user/starship/default.nix
@@ -6,21 +6,29 @@
       add_newline = true;
       directory.style = "bold blue";
       git_branch = {
-        format = "[$branch(:$remote_branch)]($style) ";
+        symbol = " ";
+        format = "[$symbol$branch(:$remote_branch)]($style) ";
         style = "green";
       };
+      git_metrics.disabled = false;
       git_status = {
-        # format = "(\[$all_status[$ahead_behind]\]($style) )";
         modified = "!\($count\)";
         staged = "+\($count\)";
         untracked = "?\($count\)";
+        renamed = "»\($count\)";
         ahead = "⇡\($count\)";
         diverged = "⇕⇡\($ahead_count\)⇣\($behind_count\)";
         behind = "⇣\($count\)";
+        stashed = "📦\($count\)";
+        conflicted = "🏳\($count\)";
       };
       cmd_duration = {
-        format = "[$duration]($style) ";
-        style = "yellow";
+        min_time = 2000;
+        format = "[ $duration]($style) ";
+        style = "white";
+      };
+      gcloud = {
+        format = "[$symbol$active]($style) ";
       };
     };
   };
