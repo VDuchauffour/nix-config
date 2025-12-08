5b25d94b79a72cdc7cfe3779d606617bfc5481a4
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-08 18:01:30 +0100

    chore: fix evaluation warning: 'system'

--- modules/system/fonts/default.nix
+++ modules/system/fonts/default.nix
@@ -7,7 +7,7 @@
   fonts = {
     packages =
       (builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts))
-      ++ (builtins.attrValues apple-fonts.packages.${pkgs.system})
+      ++ (builtins.attrValues apple-fonts.packages.${pkgs.stdenv.hostPlatform.system})
       ++ [pkgs.corefonts];
     fontDir.enable = true;
   };
