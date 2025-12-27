18a42730c66f867ea7c4a13db87e5e6b49cee326
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-27 07:20:40 +0100

    chore: update keyboard layout

--- modules/system/keyboard/default.nix
+++ modules/system/keyboard/default.nix
@@ -1,4 +1,19 @@
-# remap ² to ESC
-{
-  services.xserver.xkb.options = "<TLDE>:escape";
+{lib, ...}: {
+  # For X11/Wayland
+  services.xserver.xkb.extraLayouts.custom = {
+    description = "Custom layout with ² as Escape";
+    languages = ["eng"];
+    symbolsFile = builtins.toFile "custom-symbols" ''
+      partial modifier_keys
+      xkb_symbols "basic" {
+        key <TLDE> { [ Escape ] };
+      };
+    '';
+  };
+
+  # For console (TTY)
+  console.keyMap = lib.mkForce (builtins.toFile "custom-keymap.map" ''
+    keymaps 0-127
+    keycode 41 = Escape
+  '');
 }
