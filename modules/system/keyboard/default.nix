e5e87c06c223d90cbd8af9f3ac4d8193dd3303f9
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-12-27 09:50:20 +0100

    chore: update keyboard layout

--- modules/system/keyboard/default.nix
+++ modules/system/keyboard/default.nix
@@ -1,19 +1,31 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   # For X11/Wayland
-  services.xserver.xkb.extraLayouts.custom = {
-    description = "Custom layout with ² as Escape";
-    languages = ["eng"];
-    symbolsFile = builtins.toFile "custom-symbols" ''
-      partial modifier_keys
-      xkb_symbols "basic" {
-        key <TLDE> { [ Escape ] };
+  services.xserver = {
+    xkb = {
+      layout = lib.mkForce "fr-custom";
+      options = "lv3:ralt_switch";
+      extraLayouts.fr-custom = {
+        description = "French AZERTY with ² as Escape";
+        languages = ["fra"];
+        symbolsFile = builtins.toFile "fr-custom-symbols" ''
+          default partial alphanumeric_keys
+          xkb_symbols "basic" {
+            include "fr(latin9)"
+            key <TLDE> { [ Escape, Escape ] };
+          };
+        '';
       };
-    '';
+    };
   };
 
   # For console (TTY)
-  console.keyMap = lib.mkForce (builtins.toFile "custom-keymap.map" ''
+  console.keyMap = lib.mkForce (pkgs.writeText "custom-keymap.map" ''
     keymaps 0-127
     keycode 41 = Escape
+    include "${pkgs.kbd}/share/keymaps/i386/azerty/fr-latin9.map.gz"
   '');
 }
