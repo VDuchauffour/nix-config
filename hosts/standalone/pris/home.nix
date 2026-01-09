03cf47001ea8cd6c02b6f696a766546ea8e82c7c
Vincent Duchauffour <vincent.duchauffour@proton.me>
2026-01-09 11:55:21 +0100

    remove comment

--- hosts/standalone/pris/home.nix
+++ hosts/standalone/pris/home.nix
@@ -5,8 +5,5 @@
   vars,
   ...
 }: {
-  # Standalone home-manager configuration for Ubuntu/non-NixOS Linux
-  # Add pris-specific home-manager settings here
-
   home.homeDirectory = lib.mkForce "/home/${vars.userName}";
 }
