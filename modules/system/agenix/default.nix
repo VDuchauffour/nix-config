4df80517fafef7ad551c6ccc1c89806eb85487f6
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2026-02-19 15:06:01 +0100

    chore: add newt blueprint (#70)

--- modules/system/agenix/default.nix
+++ modules/system/agenix/default.nix
@@ -1,6 +1,7 @@
 {inputs, ...}: {
   age = {
     secrets = {
+      publicDomainName.file = "${inputs.secrets}/secrets/public-domain-name.age";
       sambaPassword.file = "${inputs.secrets}/secrets/sambaPassword.age";
       upsPassword.file = "${inputs.secrets}/secrets/upsPassword.age";
       pangolin-newt.file = "${inputs.secrets}/secrets/pangolin-newt.age";
