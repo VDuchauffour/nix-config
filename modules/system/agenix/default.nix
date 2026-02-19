075d026239c495e8774824fd322d65f0a7eb5e84
Vincent Duchauffour <vincent.duchauffour@proton.me>
2026-02-19 14:23:40 +0100

    feat: add openclaw

--- modules/system/agenix/default.nix
+++ modules/system/agenix/default.nix
@@ -4,6 +4,9 @@
       sambaPassword.file = "${inputs.secrets}/secrets/sambaPassword.age";
       upsPassword.file = "${inputs.secrets}/secrets/upsPassword.age";
       pangolin-newt.file = "${inputs.secrets}/secrets/pangolin-newt.age";
+      openclaw-gateway-token.file = "${inputs.secrets}/secrets/openclaw-gateway-token.age";
+      openclaw-telegram-token.file = "${inputs.secrets}/secrets/openclaw-telegram-token.age";
+      openclaw-telegram-id.file = "${inputs.secrets}/secrets/openclaw-telegram-id.age";
     };
   };
 }
