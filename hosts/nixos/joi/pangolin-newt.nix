2f579cc4f25b6e6a6edc1db4cf024bb4eada12c2
Vincent Duchauffour <vincent.duchauffour@proton.me>
2026-02-19 01:53:43 +0100

    fix: fix newt

--- hosts/nixos/joi/pangolin-newt.nix
+++ hosts/nixos/joi/pangolin-newt.nix
@@ -2,10 +2,6 @@
   services.newt = {
     enable = true;
     environmentFile = config.age.secrets.pangolin-newt.path;
-    settings = {
-      endpoint = "{{ .Envs.ENDPOINT }}";
-      id = "{{ .Envs.ID }}";
-      secret = "{{ .Envs.SECRET }}";
-    };
+    settings = {};
   };
 }
