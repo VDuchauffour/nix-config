b3e29b94952b7260a8cff85bd80db54a022ebaad
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2026-02-25 18:53:54 +0100

    fix(dnsmasq): handle case where resources arent available and avoid multiple dnsmasq instances (#72)

--- hosts/nixos/dnsmasq.nix
+++ hosts/nixos/dnsmasq.nix
@@ -1,9 +1,10 @@
 {
-  networking.networkmanager.dns = "dnsmasq";
+  networking.networkmanager.dns = "none";
   networking.resolvconf.enable = false;
   services.dnsmasq = {
     enable = true;
     settings = {
+      bind-dynamic = true;
       domain = "home.arpa";
       address = [
         "/home.arpa/192.168.1.18" # wildcard for *.home.arpa
@@ -14,6 +15,13 @@
       ];
       listen-address = ["127.0.0.1" "192.168.1.18"];
       server = ["1.1.1.1" "1.0.0.1" "/bytel.fr/192.168.1.254"];
+    };
+  };
+  systemd.services.dnsmasq = {
+    serviceConfig = {
+      RestartSec = "5s";
+      StartLimitIntervalSec = 120;
+      StartLimitBurst = 10;
     };
   };
 }
