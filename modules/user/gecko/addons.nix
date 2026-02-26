e11c7b531dafd2d45fcea97bcb51d2c70832f07b
Vincent Duchauffour <vincent.duchauffour@proton.me>
2026-02-26 22:05:20 +0100

    feat(gecko): add proton pass and passbolt to geck extensions

--- modules/user/gecko/addons.nix
+++ modules/user/gecko/addons.nix
@@ -90,4 +90,18 @@
     default_area = "menupanel";
     private_browsing = true;
   };
+  # Passbolt
+  "passbolt@passbolt.com" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/passbolt/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "navbar";
+    private_browsing = true;
+  };
+  # Proton Pass
+  "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+    installation_mode = "force_installed";
+    default_area = "navbar";
+    private_browsing = true;
+  };
 }
