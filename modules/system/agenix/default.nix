0deb9b1225f1da7695d91f285f902d5815399127
Vincent Duchauffour <vincent.duchauffour@proton.me>
2026-02-27 15:03:57 +0100

    feat: add gh and n8n mcps to opencode

--- modules/system/agenix/default.nix
+++ modules/system/agenix/default.nix
@@ -5,6 +5,14 @@
       sambaPassword.file = "${inputs.secrets}/secrets/sambaPassword.age";
       upsPassword.file = "${inputs.secrets}/secrets/upsPassword.age";
       pangolin-newt.file = "${inputs.secrets}/secrets/pangolin-newt.age";
+      n8n-api-key = {
+        file = "${inputs.secrets}/secrets/n8n-api-key.age";
+        owner = "k";
+      };
+      gh-api-key = {
+        file = "${inputs.secrets}/secrets/gh-api-key.age";
+        owner = "k";
+      };
     };
   };
 }
